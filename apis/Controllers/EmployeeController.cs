92ad990d34099055b2aef23ba025718935662792
rohitladhar <rohit.ladhar10@gmail.com>
2024-02-08 19:08:52 +0000

    dotnet apis with postgres database

--- apis/Controllers/EmployeeController.cs
+++ apis/Controllers/EmployeeController.cs
@@ -21,6 +21,31 @@ namespace apis.Controllers
             await _employeeRepository.AddEmployee(model);
             return Ok();
         }
+        [HttpGet]
+        public async Task<ActionResult> GetEmployeeList()
+        {
+            var employeeList = await _employeeRepository.GetAllEmployee();
+            return Ok(employeeList);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetEmployeebyId([FromRoute] int id)
+        {
+            var employee = await _employeeRepository.GetEmployeeById(id);
+            return Ok(employee);
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateEmployee([FromRoute] int id, [FromBody] Employee model)
+        {
+            await _employeeRepository.UpdateEmployee(id, model);
+            return Ok();
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteEmployee([FromRoute] int id)
+        {
+            await _employeeRepository.DeleteEMployeeById(id);
+            return Ok();
+        }
+        
     }
 
 }
--- apis/Data/EmployeeRepository.cs
+++ apis/Data/EmployeeRepository.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using WebApplication15.Data;
 
 namespace apis.Data
@@ -13,6 +14,33 @@ namespace apis.Data
         public async Task AddEmployee(Employee employee)
         {
             await _appDbContext.Set<Employee>().AddAsync(employee);
+            await _appDbContext.SaveChangesAsync();
+        }
+        public async Task<List<Employee>> GetAllEmployee()
+        {
+            return await _appDbContext.Employees.ToListAsync();
+        }
+        public async Task<Employee> GetEmployeeById(int id)
+        {
+            return await _appDbContext.Employees.FindAsync(id);
+        }
+        public async Task UpdateEmployee(int id, Employee model)
+        {
+            var employee = await _appDbContext.Employees.FindAsync(id);
+            if(employee == null)
+            {
+                throw new Exception("Employee is not found");
+            }
+            employee.Name = model.Name;
+            employee.Phone = model.Phone;
+            employee.Age = model.Age;
+            employee.Salary = model.Salary;
+            await _appDbContext.SaveChangesAsync();
+        }
+        public async Task DeleteEMployeeById(int id)
+        {
+            var employee = await _appDbContext.Employees.FindAsync(id) ?? throw new Exception("Employee is not found");
+            _appDbContext.Employees.Remove(employee);
             await _appDbContext.SaveChangesAsync();
         }
     }
